dfb28c324b5c081b8663cce825590a7ec08ce266
abhishekchauhan9036 <abhishekchauhan9036@gmail.com>
2026-01-15 12:13:20 +0530

    MU

---
+++ Lecture CodeBase/4EN2/Lecture1.java
@@ -0,0 +1,16 @@
+class Lecture1 {
+    public static void main(String args[]){
+        int a = 10;
+        float pi = 3.1416f;
+        double pie = 3.1416;
+        Boolean isTrue = true;
+        char ch = 'A';
+        String name = "Ritika";
+        System.out.println(a);
+        System.out.println(pi);
+        System.out.println(pie);
+        System.out.println(isTrue);
+        System.out.println(ch);
+        System.out.println(name);
+    }
+}
