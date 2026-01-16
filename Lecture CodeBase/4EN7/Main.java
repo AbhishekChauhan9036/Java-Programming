c4da2982e55a7d58d4455b666dd7c2ac16b7bf12
abhishekchauhan9036 <abhishekchauhan9036@gmail.com>
2026-01-16 10:04:28 +0530

    MU

---
+++ Lecture CodeBase/4EN7/Main.java
@@ -0,0 +1,10 @@
+class Main{
+    public static void main(String args[]){
+        System.out.print("Hello World!");
+        System.out.println("Hello World!");
+        float pi = 3.1415f;
+        double salary = 732.83873;
+        System.out.println(pi);
+        System.out.println(salary);
+    }
+}
