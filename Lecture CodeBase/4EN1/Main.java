c7133d0fba5e06addda544e6c56c9734c1d314e7
abhishekchauhan9036 <abhishekchauhan9036@gmail.com>
2026-01-19 18:31:41 +0530

    MU

--- Lecture CodeBase/4EN1/Main.java
+++ Lecture CodeBase/4EN1/Main.java
@@ -1,7 +1,16 @@
 class Main{
     public static void main(String args[]){
-        System.out.print("Hello Wolrd!");
-        float pi = 31.1416f;
-        long a = 782337;
+        int a = 10;
+        float pi = 3.1416f;
+        double pie = 3.1416;
+        Boolean isTrue = true;
+        char ch = 'A';
+        String name = "Ritika";
+        System.out.println(a);
+        System.out.println(pi);
+        System.out.println(pie);
+        System.out.println(isTrue);
+        System.out.println(ch);
+        System.out.println(name);
     }
 }
