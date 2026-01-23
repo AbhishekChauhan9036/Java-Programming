1e0a074a2e67bf5f5fa1ed08d32c09a8c83a2625
abhishekchauhan9036 <abhishekchauhan9036@gmail.com>
2026-01-23 07:36:49 +0530

    MU

--- Lecture CodeBase/4EN2/Lecture2.java
+++ Lecture CodeBase/4EN2/Lecture2.java
@@ -1,26 +1,26 @@
 import java.util.Scanner;
 class Lecture2{
     public static void main(String args[]){
-        // for(int i=1;i<=5;i++){
-        //     System.out.println(i);
-        // }
+        for(int i=1;i<=5;i++){
+            System.out.println(i);
+        }
 
-        // int i = 1;
-        // while(i<=5){
-        //     System.out.println(i);
-        //     i++;
-        // }
+        int i = 1;
+        while(i<=5){
+            System.out.println(i);
+            i++;
+        }
 
-        // do{
-        //     System.out.println(i);
-        //     i++;
-        // }while(i<=5);
-        // Scanner sc = new Scanner(System.in);
-        // int a = sc.nextInt();
-        // double pi = sc.nextDouble();
-        // float prize = sc.nextFloat();
-        // String str = sc.next();
-        // String sen = sc.nextLine();
+        do{
+            System.out.println(i);
+            i++;
+        }while(i<=5);
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        double pi = sc.nextDouble();
+        float prize = sc.nextFloat();
+        String str = sc.next();
+        String sen = sc.nextLine();
 
         for(int i=1;i<=5;i++){
             for(int j=1;j<=9;j++){
