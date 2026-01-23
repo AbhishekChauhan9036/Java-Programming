2f017da5b141e769b164c6ae72ee9957193bbbe1
abhishekchauhan9036 <abhishekchauhan9036@gmail.com>
2026-01-23 12:30:26 +0530

    MU

---
+++ Lecture CodeBase/4EN1/Lecture2.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+class Lecture2{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        float prize = sc.nextFloat();
+        double pie = sc.nextDouble();
+        String name = sc.next();
+        String sentence = sc.nextLine();
+        char ch = sc.next().charAt(0);
+
+        System.out.println(num);
+        System.out.println(prize);
+        System.out.println(pie);
+        System.out.println(name);
+        System.out.println(sentence);
+        System.out.println(ch);
+
+        for(int i=1;i<=5;i++){
+            System.out.println(i);
+        }
+
+        int i = 1;
+        while(i<=5){
+            System.out.println(i);
+            i++;
+        }
+
+        do{
+          System.out.println(i);
+            i++;  
+        }while(i<=5);
+
+
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=9;j++){
+                if(j>=6-i && j<=4+i){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println("");
+        }
+    }
+}
--- Lecture CodeBase/4EN7/Lecture1.java
+++ Lecture CodeBase/4EN7/Lecture1.java
@@ -1,4 +1,4 @@
-class Main{
+class Lecture1{
     public static void main(String args[]){
         System.out.print("Hello World!");
         System.out.println("Hello World!");
