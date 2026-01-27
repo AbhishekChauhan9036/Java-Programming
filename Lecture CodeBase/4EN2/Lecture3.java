0a12c7ea63e16337459fd95082d0d312f910dae8
abhishekchauhan9036 <abhishekchauhan9036@gmail.com>
2026-01-27 07:45:52 +0530

    MU

--- Lecture CodeBase/4EN2/Lecture3.java
+++ Lecture CodeBase/4EN2/Lecture3.java
@@ -4,36 +4,41 @@
 import java.util.Scanner;
 public class Lecture3{
     public static void main(String []args){
-        // int arr[] = {1,2,3,4,5};
-        // System.out.println(arr[4]);
+        // Static Array 
+        int arr[] = {1,2,3,4,5};
+        System.out.println(arr[4]);
 
-        // int []brr = new int[5];
-        // brr[0] = 1;
-        // brr[1] = 2;
-        // brr[2] = 3;
-        // brr[3] = 4;
-        // brr[4] = 5;
-        // for(int i=0;i<brr.length;i++){
-        //     System.out.print(brr[i]+" ");
-        // }
+        int []brr = new int[5];
+        brr[0] = 1;
+        brr[1] = 2;
+        brr[2] = 3;
+        brr[3] = 4;
+        brr[4] = 5;
+        for(int i=0;i<brr.length;i++){
+            System.out.print(brr[i]+" ");
+        }
 
+        // User input in Array
         Scanner sc = new Scanner(System.in);
-        // System.out.print("Enter the Size of an Array: ");
-        // int n = sc.nextInt();
-        // int array[] = new int[n];
+        System.out.print("Enter the Size of an Array: ");
+        int n = sc.nextInt();
+        int array[] = new int[n];
 
-        // System.out.print("Enter the Elements of an Array: ");
-        // for(int i=0;i<n;i++){
-        //     array[i] = sc.nextInt();
-        // }
+        System.out.print("Enter the Elements of an Array: ");
+        for(int i=0;i<n;i++){
+            array[i] = sc.nextInt();
+        }
 
-        // for(int i=0;i<n;i++){
-        //     System.out.print(array[i]+" ");
-        // }
+        for(int i=0;i<n;i++){
+            System.out.print(array[i]+" ");
+        }
 
+        // 2D Array
         int mat[][] = {{1,2},{3,4}};
         System.out.println(mat[1][0]);
-   
+
+
+        //User input in 2D Array 
         int r = sc.nextInt();
         int c = sc.nextInt();
         int matrix[][] = new int[r][c];
