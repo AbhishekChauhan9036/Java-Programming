4e642540f4038752a133d6c9159f46dcd9350647
abhishekchauhan9036 <abhishekchauhan9036@gmail.com>
2026-02-02 17:13:56 +0530

    MU

--- Lecture CodeBase/4EN8/Lecture2.java
+++ Lecture CodeBase/4EN8/Lecture2.java
@@ -1,19 +1,19 @@
 public class Lecture2{
     public static void main(String...args){
-        // for(int i=1;i<=5;i++){
-        //     System.out.println(i);
-        // }
+        for(int i=1;i<=5;i++){
+            System.out.println(i);
+        }
 
-        // int i = 1;
-        // while(i<=5){
-        //     System.out.println(i);
-        //     i++;
-        // }
+        int i = 1;
+        while(i<=5){
+            System.out.println(i);
+            i++;
+        }
 
-        // do{
-        //    System.out.println(i);
-        //     i++; 
-        // }while(i<=5);
+        do{
+           System.out.println(i);
+            i++; 
+        }while(i<=5);
 
 
         for(int i=1;i<=5;i++){
