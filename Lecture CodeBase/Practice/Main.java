2daf4317715897b69f1d0a0423b32865ba15917d
abhishekchauhan9036 <abhishekchauhan9036@gmail.com>
2026-02-08 12:21:57 +0530

    MU

--- Lecture CodeBase/Practice/Main.java
+++ Lecture CodeBase/Practice/Main.java
@@ -1,5 +1,20 @@
+class Animal {
+    void speak() {
+        System.out.println("The animal makes a sound");
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    void speak() {
+        System.out.println("The dog barks");
+    }
+}
+
 class Main {
     public static void main(String[] args) {
+        Dog myDog = new Dog();
+        myDog.speak();
         System.out.println("Hello");
         System.out.println("World");
     }
