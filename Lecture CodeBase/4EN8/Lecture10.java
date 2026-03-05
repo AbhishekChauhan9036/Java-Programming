d1fcceaa411fda55a3b258b2428469f0c7ce5dc0
abhishekchauhan9036 <abhishekchauhan9036@gmail.com>
2026-03-05 13:45:35 +0530

    MU

---
+++ Lecture CodeBase/4EN8/Lecture10.java
@@ -0,0 +1,9 @@
+public class Lecture10{
+    public static void main(String[] args){
+       int arr[] ={10,34,25,6,8,60};
+       int low = 0;
+       int high = arr.length-1;
+       int mid = (low+high)/2;
+       System.out.println(arr[mid]);
+    }
+}
