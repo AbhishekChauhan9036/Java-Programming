66e04435577325c13e05218a73ef88bdeba7e124
abhishekchauhan9036 <abhishekchauhan9036@gmail.com>
2026-03-12 09:26:16 +0530

    MU

---
+++ Lecture CodeBase/Practice/FrameExample.java
@@ -0,0 +1,11 @@
+import javax.swing.*; 
+public class FrameExample { 
+    public static void main(String[] args) { 
+        // Create a frame with a title 
+        JFrame frame = new JFrame("Swing Example"); 
+        // Set size of window 
+        frame.setSize(400,300); 
+        // Make window visible 
+        frame.setVisible(true); 
+    } 
+}
