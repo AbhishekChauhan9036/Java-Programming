22642d4f60ea16fb958cbf3301ec4c739fc8c4a9
abhishekchauhan9036 <abhishekchauhan9036@gmail.com>
2026-03-28 12:54:27 +0530

    MU

--- Lecture CodeBase/4EN1/Lecture2.java
+++ Lecture CodeBase/4EN1/Lecture2.java
@@ -32,7 +32,7 @@ class Lecture2{
         }while(i<=5);
 
 
-        for(int i=1;i<=5;i++){
+        for(i=1;i<=5;i++){
             for(int j=1;j<=9;j++){
                 if(j>=6-i && j<=4+i){
                     System.out.print("*");
