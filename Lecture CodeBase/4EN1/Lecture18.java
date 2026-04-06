35365ea1a4b3e7b4a73fb4f5066ba5dbfc9ad354
abhishekchauhan9036 <abhishekchauhan9036@gmail.com>
2026-04-06 12:37:39 +0530

    MU

---
+++ Lecture CodeBase/4EN1/Lecture18.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+public class Lecture18{
+    public static void main(String[] args) {
+        JFrame jf = new JFrame("Swing Example");
+        jf.setSize(600,500);
+        jf.setVisible(true);
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
