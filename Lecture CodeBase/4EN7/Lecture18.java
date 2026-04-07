2ab9c69bc44e93fa0f12973ba1dcafc029f7da6e
abhishekchauhan9036 <abhishekchauhan9036@gmail.com>
2026-04-07 10:23:56 +0530

    MU

---
+++ Lecture CodeBase/4EN7/Lecture18.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+public class Lecture18 {
+    public static void main(String[] args) {
+        JFrame jf = new JFrame("Notedpad");
+        jf.setSize(300,300);
+        jf.setVisible(true);
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
